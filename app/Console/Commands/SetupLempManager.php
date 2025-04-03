5570a48a1c8011062cd756968de1093c7f13b660
Mohyaddin Saleh <mo7y.66@gmail.com>
2025-04-03 11:04:51 +0300

    Finalized LEMP manager app setup command.

--- app/Console/Commands/SetupLempManager.php
+++ app/Console/Commands/SetupLempManager.php
@@ -23,9 +23,15 @@ class SetupLempManager extends Command{
      * Execute the console command.
      */
     public function handle(){
+        $this->info('Setting up server configurations..');
+        $output = `mkcert lemp.docker && docker cp lemp.docker.pem lemp-nginx-1:/etc/nginx/ssl/ && docker cp lemp.docker-key.pem lemp-nginx-1:/etc/nginx/ssl/ && rm -f lemp.docker*.pem`;
+        $output .= `docker exec -w /etc/nginx lemp-nginx-1 sed -i 's/#listen/listen/g ; s/#ssl/ssl/g' nginx.conf && docker exec lemp-nginx-1 nginx -s reload`;
+        $this->comment($output);
+        $this->comment('Server configurations setup complete.');
+
         $this->info('Setting up database..');
         $password = env('DB_PASSWORD');
-        $dbCheck = trim(`docker exec lemp-mariadb-1 mysql -u root -p$password -N -e 'show databases;' | grep --color=none {$this->project->database}`);
+        $dbCheck = trim(`docker exec lemp-mariadb-1 mysql -u root -p$password -N -e 'show databases;' | grep --color=none lemp`);
         if($dbCheck != 'lemp'){
             $output = `docker exec lemp-mariadb-1 mysql -u root -p$password -e 'create database lemp;'`;
             $output .= `php artisan migrate`;
